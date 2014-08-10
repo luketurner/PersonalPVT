8acb2236243795e7eeb7cba2a99ba345349dd8fd
Luke Turner <github@luketurner.org>
2014-08-09 20:53:05 -0500

    Work on results data and view

--- results/results-controller.js
+++ results/results-controller.js
@@ -1,6 +1,8 @@
 angular.module('pvtApp').controller('ResultsCtrl', ['$scope', "trialData", function ($scope, trialData) {
-    $scope.times = trialData.times;
+    $scope.trialData = trialData;
+
     $scope.chartData = [];
     for (var i = 0; i < 100; i++) { $scope.chartData.push( Math.random() * 1000); } // some junk data for testing
+    trialData.times = $scope.chartData;
     $scope.chartSettings = {};
 }]);
